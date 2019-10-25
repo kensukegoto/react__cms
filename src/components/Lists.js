26eaed9257b5203ed936e1dd63ebdd218c575a40
kensuke goto <utogoken2@yahoo.co.jp>
2019-10-26 00:21:55 +0900

    (dev) data.jsを元に作品一覧を表示する

--- src/components/Lists.js
+++ src/components/Lists.js
@@ -1,14 +1,82 @@
 import React,{ useContext } from 'react'
 
+import {
+  Paper,
+  Grid,
+  List,
+  ListItem,
+  ListItemText,
+  ListItemSecondaryAction,
+  IconButton
+} from '@material-ui/core';
+import {
+  Edit as EditIcon,
+  Delete as DeleteIcon
+} from '@material-ui/icons';
+
+import { makeStyles } from '@material-ui/core/styles';
+
 import  AppContext from '../contexts/AppContext'
+
+
+
+
+
+const useStyles = makeStyles(theme => ({
+  paper: {
+    padding: 20,
+    marginTop: 10,
+    marginBottom: 10,
+    height: 500,
+    overflowY: "auto"
+  }
+}))
+
+
 
 const  Lists = () => {
 
+  const classes = useStyles()
+
   const {works} = useContext(AppContext)
-  console.log(works)
+
   return (
     <>
-    俺だ
+      <Grid container spacing={2}>
+        <Grid item sm>
+          <Paper className={classes.paper}>
+            <List>
+          {
+            works.map(work => {
+
+              return (
+                <ListItem button key={work}>
+                  <Grid container>
+                    <Grid item sm={9}>
+                    <ListItemText primary={`${work.h4}`} />
+                    </Grid>
+                    <Grid item sm>
+                      <ListItemSecondaryAction>
+                        <IconButton aria-label="delete">
+                          <DeleteIcon />
+                        </IconButton>
+                        <IconButton aria-label="edit">
+                          <EditIcon />
+                        </IconButton>
+                      </ListItemSecondaryAction>
+                    </Grid>
+                  </Grid>
+                </ListItem>
+              )
+            })
+          }
+          </List>
+          </Paper>
+        </Grid>
+        <Grid item sm>
+          <Paper className={classes.paper}></Paper>
+        </Grid>
+      </Grid>
     </>
   )
 
